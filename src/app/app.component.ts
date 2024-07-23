217c46de1adfc96379faa6f8641f15d5e0206903
Exp <17885980+exp111@users.noreply.github.com>
2024-07-23 22:10:59 +0200

    sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import {RouterOutlet} from '@angular/router';
 import {Posting, PostingSource} from "./model/posting";
 import {FundgrubeService} from "./service/fundgrube.service";
 import {NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+enum SortBy {
+  Name,
+  Price
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf],
+  imports: [RouterOutlet, NgForOf, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,8 +21,15 @@ export class AppComponent implements OnInit {
   title = 'FundgrubeSpiele';
   allPostings: Posting[] = [];
   filteredPostings: Posting[] = [];
+  //TODO: save all filters here and use observe function
+  searchFilter: string = "";
+  sortBy: SortBy = SortBy.Price;
+  ascendingSort: boolean = true;
+  showMM: boolean = true;
+  showSaturn: boolean = true;
 
   constructor(public fundgrubeService: FundgrubeService) {
+    (window as any).app = this;
   }
 
   ngOnInit(): void {
@@ -26,8 +39,41 @@ export class AppComponent implements OnInit {
       this.fundgrubeService.GetSaturn().then(postings => this.allPostings.push(...postings))
     ])
       .then(() => {
-        this.filteredPostings = [...this.allPostings];
+        this.filter();
       })
+  }
+
+  // gets the entries of an enum, ignoring any inbuilt non number enusm
+  enumEntries(en: any) {
+    return Object.entries(en).filter(e => isNaN(e[0] as any))
+  }
+
+  filter() {
+    this.filteredPostings = this.allPostings.filter(p => {
+      if (!this.showMM && p.source == PostingSource.MediaMarkt) {
+        return false;
+      }
+      if (!this.showSaturn && p.source == PostingSource.Saturn) {
+        return false;
+      }
+
+      if (!p.name.toLowerCase().includes(this.searchFilter.toLowerCase())) {
+        return false;
+      }
+      return true;
+    })
+      .sort((a, b) => {
+        let first = this.ascendingSort ? a : b;
+        let other = this.ascendingSort ? b : a;
+        // we have to cast to number because the select sets it to it
+        switch (Number(this.sortBy)) {
+          case Number(SortBy.Name):
+            return first.name.localeCompare(other.name, undefined, {sensitivity: "base"});
+          case Number(SortBy.Price):
+            return first.price - other.price;
+        }
+        throw new Error(`Unknown sort type: ${this.sortBy}`);
+      });
   }
 
   onSourceChecked(event: any, source: PostingSource) {
@@ -45,4 +91,5 @@ export class AppComponent implements OnInit {
   }
 
   protected readonly PostingSource = PostingSource;
+  protected readonly SortBy = SortBy;
 }
